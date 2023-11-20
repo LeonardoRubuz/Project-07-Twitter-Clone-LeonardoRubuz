fe82cbd3808bfaac3a144f672ed01130329a858f
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-11-20 11:44:06 +0100

    inner editor structure done

--- src/components/inner-tweet-editor.jsx
+++ src/components/inner-tweet-editor.jsx
@@ -1,7 +1,13 @@
 function InnerEditor() {
     return ( 
         <form action="" className="tweet-editor-form">
-            
+            <input type="text" className="tweet-editor-input" />
+            <div className="tweet-editor-buttons">
+                <ul className="tweet-editor-actions">
+                    <li></li>
+                </ul>
+                <button className="button">Tweet</button>
+            </div>
         </form>
      );
 }
