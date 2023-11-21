95c508d21db8f68d32f582e48d8e3f8790b5a117
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-11-21 15:54:24 +0100

    profile-photo rendered

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,7 +1,9 @@
+import profilePhoto from "../images/profile-photo.png";
+
 function Avatar() {
     return ( 
         <div className="avatar">
-            <img src="" alt="" />
+            <img src={profilePhoto} alt="" />
         </div>
      );
 }
