8e6d0939523f7895fe9093d83104b5a5be53c420
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-11-21 16:27:03 +0100

    react-icons installed and tweet editor completed

--- src/components/inner-tweet-editor.jsx
+++ src/components/inner-tweet-editor.jsx
@@ -1,11 +1,20 @@
+import { BiPoll, BiImage,  } from "react-icons/bi";
+import { HiOutlineGif } from "react-icons/hi2";
+import { GrEmoji } from "react-icons/gr";
+import { LuCalendarClock } from "react-icons/lu";
+
 function InnerEditor() {
     return ( 
         <form action="" className="tweet-editor-form">
             <input type="text" className="tweet-editor-input" placeholder="What's happening?" />
             <div className="tweet-editor-buttons">
-                <ul className="tweet-editor-actions">
-                    <li></li>
-                </ul>
+                <div className="tweet-editor-actions">
+                    <span><BiImage /></span>
+                    <span><HiOutlineGif /></span>
+                    <span><BiPoll /></span>
+                    <span><GrEmoji /></span>
+                    <span><LuCalendarClock /></span>
+                </div>
                 <button className="button">Tweet</button>
             </div>
         </form>
