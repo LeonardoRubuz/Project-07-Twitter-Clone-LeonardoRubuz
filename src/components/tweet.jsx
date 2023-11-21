2242ecb44b0db15d1c5b6bcb40f8cdbc84548328
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-11-22 00:43:07 +0100

    fragment removed

--- src/components/tweet.jsx
+++ src/components/tweet.jsx
@@ -2,13 +2,18 @@ import profile from '../images/tweet-profile-photo.png';
 import image from '../images/tweet-image.png';
 function Tweet() {
     return ( 
-        <>
+        <div className='tweet'>
             <div className="tweet-avatar">
                 <img src={profile} alt="" />
             </div>
             <div className="tweet-content">
                 <div className="tweet-body">
-                    <div className="tweet-title"></div>
+                    <div className="tweet-title">
+                        <span className='tweet-title-author'>John Doe</span>
+                        <span className="tweet-title-details">@JohnDoe</span>
+                        <span className="tweet-title-details">.</span>
+                        <span className="tweet-title-details">April 3</span>
+                    </div>
                     <div className="tweet-text">Lorem ipsum dolor sit amet consectetur adipisicing. </div>
                     <div className="tweet-image">
                         <img src={image} alt="" />
@@ -16,7 +21,7 @@ function Tweet() {
                 </div>
                 <div className="tweet-actions"></div>
             </div>
-        </>
+        </div>
      );
 }
 
