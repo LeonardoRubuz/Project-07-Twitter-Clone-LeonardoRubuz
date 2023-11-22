dd69c0dcd04747faf46ddb2f336feb4de022f889
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-11-22 13:41:38 +0100

    tweet editor actions buttons fixed

--- src/components/inner-tweet-editor.jsx
+++ src/components/inner-tweet-editor.jsx
@@ -1,7 +1,8 @@
-import { BiPoll, BiImage,  } from "react-icons/bi";
-import { HiOutlineGif } from "react-icons/hi2";
-import { GrEmoji } from "react-icons/gr";
-import { LuCalendarClock } from "react-icons/lu";
+import media from "../../public/svg/Media.svg";
+import emoji from "../../public/svg/Emoji.svg";
+import gif from "../../public/svg/Gif.svg";
+import poll from "../../public/svg/Poll.svg";
+import schedule from "../../public/svg/Schedule.svg";
 
 function InnerEditor() {
     return ( 
@@ -9,11 +10,11 @@ function InnerEditor() {
             <input type="text" className="tweet-editor-input" placeholder="What's happening?" />
             <div className="tweet-editor-buttons">
                 <div className="tweet-editor-actions">
-                    <span><BiImage /></span>
-                    <span><HiOutlineGif /></span>
-                    <span><BiPoll /></span>
-                    <span><GrEmoji /></span>
-                    <span><LuCalendarClock /></span>
+                    <button><img src={media} alt="" /></button>
+                    <button><img src={gif} alt="" /></button>
+                    <button><img src={poll} alt="" /></button>
+                    <button><img src={emoji} alt="" /></button>
+                    <button><img src={schedule} alt="" /></button>
                 </div>
                 <button className="button">Tweet</button>
             </div>
