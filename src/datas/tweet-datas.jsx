a57341594a674ca3beaada3281d6b6c117b5af79
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-11-22 09:10:33 +0100

    tweet datas file created

---
+++ src/datas/tweet-datas.jsx
@@ -0,0 +1,50 @@
+import cnn from '../images/tweet-profile-photo.png';
+import image from '../images/tweet-image.png';
+
+const datas = [
+    {
+        id  : 1 ,
+        avatar : {},
+        author : "",
+        username : "",
+        date : "",
+        content : "",
+        image : {},
+    },
+    {
+        id  : 2 ,
+        avatar : {},
+        author : "",
+        username : "",
+        date : "",
+        content : "",
+        image : {},
+    },
+    {
+        id  : 3 ,
+        avatar : {},
+        author : "",
+        username : "",
+        date : "",
+        content : "",
+        image : {},
+    },
+    {
+        id  : 4 ,
+        avatar : {},
+        author : "",
+        username : "",
+        date : "",
+        content : "",
+        image : {},
+    },
+    {
+        id  : 5 ,
+        avatar : {},
+        author : "",
+        username : "",
+        date : "",
+        content : "",
+        image : {},
+    }
+]
