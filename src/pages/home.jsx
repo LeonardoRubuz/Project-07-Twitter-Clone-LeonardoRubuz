21545dbcdd01172209431248b0514584ff32c7d0
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-12-04 12:08:30 +0100

    react router package added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 function Home({children}) {
   return (
+    <>
+      <div className="sidebar">
+        
+      </div>
     <main className="timeline">
       {children}
     </main>
+    <div className="trends">
+      
+    </div>
+    </>
   );
 }
 
